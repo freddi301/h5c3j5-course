b6009db0c26397c28f4680a7fc3f59bed4652489
Frederik Batuna <gobi301@gmail.com>
2017-03-23 08:03:39 -0700

    edit

--- exercises/arrays/reimplementMapWithREduce.js
+++ exercises/arrays/reimplementMapWithREduce.js
@@ -7,3 +7,5 @@ function mioMap(array, funzione){
 [1,2,3].map(function(item){ return item / 2 })
 
 mioMap([1,2,3], function(item){ return item / 2 })
+
+// reimplement also https://lodash.com/docs/4.17.4#groupBy
